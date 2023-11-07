c2ba348bc181c2fdf909fca90e1f0b51d4fdef5a
Bharti277 <bhartideependra277@gmail.com>
2023-11-07 12:59:53 +0530

    single movie card uodated

--- src/components/SingleMovie.jsx
+++ src/components/SingleMovie.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
+const API_URL = `https://omdbapi.com/?apikey=a6f0d8b6`;
+
 const SingleMovie = () => {
+  const [movie, setMovie] = useState("");
   const { id } = useParams();
-  return <div>SingleMovie {id}</div>;
+  const getMovies = async (api) => {
+    try {
+      const res = await fetch(api);
+      const data = await res.json();
+      console.log(data);
+      if (data.Response === "True") {
+        setMovie(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getMovies(`${API_URL}&i=${id}`);
+  }, [id]);
+
+  console.log(movie, "movieid");
+  return (
+    <div className="single__movie__card">
+      <h2>{movie.Title}</h2>
+      <img src={movie.Poster} alt={movie.Poster} />
+    </div>
+  );
 };
 
 export default SingleMovie;
